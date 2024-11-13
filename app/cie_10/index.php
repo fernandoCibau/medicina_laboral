cb55039d348fa12d6cba9a720ed03c9804d90ef8
fernando cibau <fernandocibau89@gmail.com>
2024-11-13 00:08:33 -0300

    SESION C-10

--- app/cie_10/index.php
+++ app/cie_10/index.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+    if( !isset($_SESSION['idUsuario']) ){
+        header( "location: ../index.php");
+        exit;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
